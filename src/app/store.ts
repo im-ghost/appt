759fc7730c6c1b757850140059b9d8ec3f8ac9da
Ghost <culestfrosh@gmail.com>
2023-02-03 23:36:36 +0100

    added appointment and user to the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,10 +4,14 @@ import {
   Action
  } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/user/userSlice';
+import appointmentReducer from '../features/appointments/appointmentSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    appointment: appointmentReducer,
+    user: userReducer,
   },
 });
 
