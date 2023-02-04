05e3ea7c41e73f03144a04b8c6ed0bc38765fd47
Ghost <culestfrosh@gmail.com>
2023-02-04 13:40:58 +0100

    created a fetching function

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,22 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+async function request<TResponse>(
+  url: string, 
+  config: RequestInit = {}
+): Promise<TResponse> {
+  const response = await fetch(url, config);
+  return await response.json();
+}
+ export const api = {
+  get: <TResponse>(url: string) => 
+  	request<TResponse>(url),
+  post: <TBody extends BodyInit, TResponse>(url: string, body: TBody) => 
+  	request<TResponse>(url, { method: 'POST', body }),
+   put: <TBody extends BodyInit, TResponse>(url: string, body: TBody) => 
+  	request<TResponse>(url, { method: 'PUT', body }),
+  delete: <TResponse>(url: string, id: string) => 
+  	request<TResponse>(url, { method: 'DELETE', body:id }),
+}
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
